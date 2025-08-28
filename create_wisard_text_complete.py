6fa79b0abfa2618bd5aed18152423e231dd4e1f9
KatHellg <katja@scaleoutsystems.com>
2025-08-28 10:29:28 +0200

    create text fix

--- create_wisard_text_complete.py
+++ create_wisard_text_complete.py
@@ -1,35 +1,33 @@
 import os
 from glob import glob  # You forgot this import
-path_to_datasets = "/home/mattias/Documents/projects/Wisard_usecase/datasets"
+path_to_datasets = "/Users/katjahellgren/YOLOv8-pt-fedn/split_datasets"
 
-for dataset in os.listdir(path_to_datasets):
-    if dataset.startswith("dataset"):
-        # TRAIN
-        image_dir = os.path.join(path_to_datasets, dataset, "train/images/")
-        output_txt = os.path.join(path_to_datasets, dataset, "train.txt")
+# TRAIN
+image_dir = os.path.join(path_to_datasets, "train/images/")
+output_txt = os.path.join(path_to_datasets, "train.txt")
 
-        image_paths = sorted(
-            glob(os.path.join(image_dir, "*.jpg")) +
-            glob(os.path.join(image_dir, "*.jpeg"))
-        )
+image_paths = sorted(
+    glob(os.path.join(image_dir, "*.jpg")) +
+    glob(os.path.join(image_dir, "*.jpeg"))
+)
 
-        with open(output_txt, "w") as f:
-            for path in image_paths:
-                f.write(os.path.abspath(path) + "\n")
+with open(output_txt, "w") as f:
+    for path in image_paths:
+        f.write(os.path.abspath(path) + "\n")
 
-        print(f"Saved {len(image_paths)} image paths to {output_txt}")
+print(f"Saved {len(image_paths)} image paths to {output_txt}")
 
-        # VALID
-        image_dir = os.path.join(path_to_datasets, dataset, "valid/images/")
-        output_txt = os.path.join(path_to_datasets, dataset, "valid.txt")
+# VALID
+image_dir = os.path.join(path_to_datasets, "valid/images/")
+output_txt = os.path.join(path_to_datasets, "valid.txt")
 
-        image_paths = sorted(
-            glob(os.path.join(image_dir, "*.jpg")) +
-            glob(os.path.join(image_dir, "*.jpeg"))
-        )
+image_paths = sorted(
+    glob(os.path.join(image_dir, "*.jpg")) +
+    glob(os.path.join(image_dir, "*.jpeg"))
+)
 
-        with open(output_txt, "w") as f:
-            for path in image_paths:
-                f.write(os.path.abspath(path) + "\n")
+with open(output_txt, "w") as f:
+    for path in image_paths:
+        f.write(os.path.abspath(path) + "\n")
 
-        print(f"Saved {len(image_paths)} image paths to {output_txt}")
+print(f"Saved {len(image_paths)} image paths to {output_txt}")
